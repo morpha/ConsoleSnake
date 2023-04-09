0296a094d6a4b7be01e63b386a4a4f7279923dfe
morpha <fruitylord@hotmail.com>
2023-04-09 18:47:42 +0200

    Quick fix: Accidental auto-complete also added an unneeded using directive

--- ConsoleSnake/Menu.cs
+++ ConsoleSnake/Menu.cs
@@ -2,7 +2,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using System.Reflection.Metadata.Ecma335;
 using System.Text;
 using System.Threading.Tasks;
 using CC = ConsoleCompanion;
